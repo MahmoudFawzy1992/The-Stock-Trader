401df2afdc332c29110e5f0ad90e4b4b1144b3f7
MahmoudFawzy1992 <mahmoud_fawzy1992@yahoo.com>
2020-09-21 20:45:15 +0200

    adding order checks

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Trade or View your portfolio</h1>
+        <h6>You may save and load your data</h6>
+        <h6>Cick on End day to begin a new day!</h6>
+        <hr>
+        <p>Your funds: {{funds}}</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    computed: {
+        funds() {
+            return this.$store.getters.funds;
+        }
+    }
+}
+</script>
